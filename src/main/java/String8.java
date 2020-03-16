e6ecb6ca5eb2882770fe7edc7e9451d187089604
Alice Borges <aliceborges@outlook.com.br>
2020-03-16 13:50:05 -0300

    Mostrando sempre a última parte após o espaço de uma string. No caso, sobrenome.

---
+++ src/main/java/String8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class String8 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        String nome, sobrenome;
+        int i;
+
+        System.out.printf("Informe um nome:\n");
+        nome = ler.nextLine();
+
+// retorna a posição da última ocorrência do espaço em branco
+// na string 'nome', ou seja, o sobrenome é a última parte de um nome
+        i = nome.lastIndexOf(' ');
+        sobrenome = nome.substring(i+1, nome.length());
+
+        System.out.printf("\nSobrenome:\n%s\n", sobrenome);
+    }
+
+}
